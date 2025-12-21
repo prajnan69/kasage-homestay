a179a7b934d68f3baf383b1bf19cf36a781d71a4
prajnan69 <prajnan@protonmail.com>
2025-12-22 00:08:51 +0530

    map testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MapPage from "./MapPage";
 import BookingPage from "./BookingPage";
+import TestMapPage from "./TestMapPage";
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<MapPage />} />
         <Route path="/booking" element={<BookingPage />} />
+        <Route path="/test" element={<TestMapPage />} />
       </Routes>
     </BrowserRouter>
   );
